111f168e6574b1d8f1b7d9df641b5da0ce88aff0
nscislowska <jaffafa.0@gmail.com>
2020-10-13 12:54:27 +0200

    snake moving and changing direction

--- public/javascripts/snake.js
+++ public/javascripts/snake.js
@@ -2,36 +2,95 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 let PARTSIZE = 10;
+let speed = 100;
+
+const LEFT_KEY = "ArrowLeft";
+const RIGHT_KEY = "ArrowRight";
+const UP_KEY = "ArrowUp";
+const DOWN_KEY = "ArrowDown";
+
 class Snake{
-    constructor() {
-        this.parts = [
-            {x: 150, y: 150},
-            {x: 140, y: 150},
-            {x: 130, y: 150},
-            {x: 120, y: 150},
-            {x: 110, y: 150},]
-        this.direction = PARTSIZE
+    constructor(length) {
+        this.build(length);
+        this.direction = {
+            x: PARTSIZE,
+            y: 0
+        }
+    }
+    build(length){
+        this.parts=[];
+        let startPoint = {
+            x: 0,
+            y: canvas.clientHeight/2
+        }
+        for(let i=0; i<length;i++){
+            this.parts.push({
+                x: startPoint.x - PARTSIZE*i,
+                y: startPoint.y
+            });
+        }
     }
     draw(){
         this.parts.forEach(this.drawPart);
     }
-    drawPart(snakePart) {
-        ctx.fillStyle = 'lightgreen';
+    drawPart(snakePart, index) {
+        if(index === 0){
+            ctx.fillStyle = 'darkgreen';
+        } else{
+            ctx.fillStyle = 'lightgreen';
+        }
         ctx.strokestyle = 'darkgreen';
         ctx.fillRect(snakePart.x, snakePart.y, PARTSIZE, PARTSIZE);
         ctx.strokeRect(snakePart.x, snakePart.y, PARTSIZE, PARTSIZE);
     }
     move(){
-        let head = {x: this.parts[0].x + PARTSIZE, y: this.parts[0].y};
+        let head = {x: this.parts[0].x + this.direction.x, y: this.parts[0].y + this.direction.y};
+        if(head.x >= canvas.clientWidth){
+            head.x = 0;
+        } else if( head.x <= -PARTSIZE){
+            head.x = canvas.clientWidth-PARTSIZE;
+
+        } else if(head.y <= -PARTSIZE){
+            head.y = canvas.clientHeight-PARTSIZE;
+        } else if(head.y >= canvas.clientHeight){
+            head.y = 0;
+        }
         this.parts.unshift(head);
         this.parts.pop();
     }
 
 }
+snake = new Snake(10);
 
-snake = new Snake();
+function changeDirection(event) {
+    const keyPressed = event.key;
+    const goingUp = snake.direction.y === -PARTSIZE;
+    const goingDown = snake.direction.y === PARTSIZE;
+    const goingRight = snake.direction.x === PARTSIZE;
+    const goingLeft = snake.direction.x === -PARTSIZE;
+
+    if (keyPressed === LEFT_KEY && !goingRight) {
+        snake.direction.x = -10;
+        snake.direction.y = 0;
+    }
+    if (keyPressed === RIGHT_KEY && !goingLeft) {
+        snake.direction.x = 10;
+        snake.direction.y = 0;
+    }
+    if (keyPressed === UP_KEY && !goingDown) {
+        snake.direction.x = 0;
+        snake.direction.y = -10;
+    }
+    if (keyPressed === DOWN_KEY && !goingUp) {
+        snake.direction.x = 0;
+        snake.direction.y = 10;
+    }
+
+}
+
+
+document.addEventListener("keydown", changeDirection);
 main();
-
 function main() {
     setTimeout(
         function onTick() {
@@ -39,7 +98,7 @@ function main() {
             snake.draw();
             snake.move();
             main();
-        },1000)
+        },speed)
 }
 
 function clearCanvas() {
